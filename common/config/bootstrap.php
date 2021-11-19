9470609fd11dcb5df482eee007e4c3c8913c8a25
hoahpu2 <34579029+hoahpu2@users.noreply.github.com>
2021-11-19 13:44:48 +0700

    rm

--- common/config/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-Yii::setAlias('@common', dirname(__DIR__));
-Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
-Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
-Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
-
-function xx($data = null){
-    echo "<pre>";print_r($data);die('11');
-}
