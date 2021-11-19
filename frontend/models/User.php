1620906295f1f87d64c0da254908a36f2ec07217
hoahpu2 <34579029+hoahpu2@users.noreply.github.com>
2021-11-19 13:54:22 +0700

    add pro

---
+++ frontend/models/User.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string|null $name
+ * @property int|null $number_phone
+ * @property string|null $sex
+ * @property string|null $email
+ * @property string|null $avatar_url
+ * @property string|null $password
+ * @property string|null $date_create
+ * @property string|null $date_update
+ * @property string|null $last_login
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['number_phone'], 'integer'],
+            [['date_create', 'date_update', 'last_login'], 'safe'],
+            [['name', 'password'], 'string', 'max' => 255],
+            [['sex'], 'string', 'max' => 10],
+            [['email', 'avatar_url'], 'string', 'max' => 100],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'number_phone' => 'Number Phone',
+            'sex' => 'Sex',
+            'email' => 'Email',
+            'avatar_url' => 'Avatar Url',
+            'password' => 'Password',
+            'date_create' => 'Date Create',
+            'date_update' => 'Date Update',
+            'last_login' => 'Last Login',
+        ];
+    }
+}
---
+++ frontend/views/user/_form.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\User */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="user-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'number_phone')->textInput() ?>
+
+    <?= $form->field($model, 'sex')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'avatar_url')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'password')->passwordInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'date_create')->textInput() ?>
+
+    <?= $form->field($model, 'date_update')->textInput() ?>
+
+    <?= $form->field($model, 'last_login')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
